df469990a6e2412d8af6662e26f331e7a1875f42
Light-UA <78278078+Light-UA@users.noreply.github.com>
2021-01-30 22:47:23 +0200

    files

---
+++ jssrc.js
@@ -0,0 +1,95 @@
+//alert("hello");
+//const lastName = "Alex";
+//let age = 14;
+
+//console.log(lastName);
+//var myName = "Alexei";
+//console.log(myName);
+
+//const isProgrammer = true;
+//console.log(isProgrammer);
+
+//const _private = "private";
+//const $ = "11"
+//console.log($);
+
+//console.log("Имя человека :" + lastName +  ",а возраст человека " +  age);
+
+//prompt(message:"Введи текст");
+
+//let currentYear = 2021;
+//let birthYear = 2006;
+
+//const age = currentYear - birthYear;
+//console.log(age);
+
+//const a = 10;
+//const b = 5;
+
+//console.log(a + b);
+//console.log(a - b);
+//console.log(a * b);
+//console.log(a / b);
+
+//console.log(++currentYear);
+//console.log(--birthYear);
+
+//let c = 32;
+//c = c + a;
+//c += a;
+//c -= a;
+//console.log(c)
+
+	//const isProgrammer = true;
+	//const name = "Alex";
+	//const age = 14;
+	//let x = null;
+	//console.log(typeof isProgrammer);
+	//console.log(typeof name);
+	//console.log(typeof age);
+	//console.log(x);
+
+	//const fullAge = 14;
+	//const birthYear = 2006;
+	//const currentYear = 2021;
+
+	//const isFullAge = (currentYear - birthYear) >= fullAge;
+	//console.log(isFullAge);
+
+
+	const courseStatus = 'ready'; //ready, fail, pending.
+	if (courseStatus === 'ready') {
+		console.log('Курс уже готов !')
+	};
+	if (courseStatus === 'pending') {
+		console.log('Курс еще в розработке !')
+	};
+	if (courseStatus === 'fail') {
+		console.log('Курс завален')
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
